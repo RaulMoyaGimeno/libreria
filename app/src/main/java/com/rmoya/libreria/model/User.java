07c3630e73478d163f733100730618d4b171e9ff
Raúl Moya Gimeno <rmoya889@gmail.com>
2023-02-15 17:47:32 +0100

    Modelo libro y usuario

--- app/src/main/java/com/rmoya/libreria/model/User.java
+++ app/src/main/java/com/rmoya/libreria/model/User.java
@@ -1,4 +1,26 @@
 package com.rmoya.libreria.model;
 
 public class User {
+
+    private final String name;
+    private final String user;
+    private final String password;
+
+    public User(String name, String user, String password) {
+        this.name = name;
+        this.user = user;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
