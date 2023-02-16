a9647e7f24226e0d0c3451b20ef0cddf085c08ae
Raúl Moya Gimeno <rmoya889@gmail.com>
2023-02-16 18:05:51 +0100

    Notificaciones

--- app/src/main/java/com/rmoya/libreria/util/Notification.java
+++ app/src/main/java/com/rmoya/libreria/util/Notification.java
@@ -1,4 +1,38 @@
 package com.rmoya.libreria.util;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.rmoya.libreria.R;
+
 public class Notification {
+
+    private static final String CHANNEL_ID = "NOTIFICATION_ID";
+    private static final int ID = 1000000;
+
+    public static void createChannel(@NonNull Context context) {
+        CharSequence name = context.getString(R.string.channel_name);
+        String description = context.getString(R.string.channel_description);
+        int importance = NotificationManager.IMPORTANCE_DEFAULT;
+        NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+        channel.setDescription(description);
+        NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(channel);
+    }
+
+    public static void notify(Context context, String textTitle, String textContent){
+        android.app.Notification notification = new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setSmallIcon(androidx.constraintlayout.widget.R.drawable.abc_ic_star_black_16dp)
+                .setContentTitle(textTitle)
+                .setContentText(textContent)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .build();
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+        notificationManager.notify(ID, notification);
+    }
 }
