80f292f4493c87ad46938ce8418049b0a8f00383
Snowboys <ismaeljg2002@gmail.com>
2023-02-17 16:41:49 +0100

    Splash Correcto

--- app/src/main/java/com/rmoya/libreria/view/MainActivity.java
+++ app/src/main/java/com/rmoya/libreria/view/MainActivity.java
@@ -2,7 +2,10 @@ package com.rmoya.libreria.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
 import com.rmoya.libreria.R;
 
@@ -12,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Handler handler = new Handler(Looper.myLooper());
+
+        handler.postDelayed(() -> {
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }, 1500);
     }
 }
