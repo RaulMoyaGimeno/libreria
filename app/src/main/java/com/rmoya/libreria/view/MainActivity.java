d89fc6bbd0f818e252885a1c974261e975ec8741
Snowboys <ismaeljg2002@gmail.com>
2023-02-22 15:38:19 +0100

    Splash with SharedPreference

--- app/src/main/java/com/rmoya/libreria/view/MainActivity.java
+++ app/src/main/java/com/rmoya/libreria/view/MainActivity.java
@@ -3,11 +3,16 @@ package com.rmoya.libreria.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.preference.PreferenceManager;
 
 import com.rmoya.libreria.R;
+import com.rmoya.libreria.bbdd.DBInit;
+
+import java.util.prefs.Preferences;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,11 +21,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        DBInit.inicializarSqlite(this,R.raw.libreria);
+
+        Class<?> clase = LoginActivity.class;
+
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+
+        if(sp.contains("name")){
+            clase = MainScreenActivity.class;
+        }
 
         Handler handler = new Handler(Looper.myLooper());
 
+        Class<?> finalClase = clase;
         handler.postDelayed(() -> {
-            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            Intent intent = new Intent(getApplicationContext(), finalClase);
             startActivity(intent);
             finish();
         }, 1500);
