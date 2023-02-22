e7a7faa18eb0526cd10a94026f290697a987e71d
Alvaro <alvaropuyobraulio@gmail.com>
2023-02-22 15:39:11 +0100

    botones pantalla principal

--- app/src/main/java/com/rmoya/libreria/view/MainScreenActivity.java
+++ app/src/main/java/com/rmoya/libreria/view/MainScreenActivity.java
@@ -2,11 +2,11 @@ package com.rmoya.libreria.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.widget.Button;
-import android.widget.ImageButton;
 
 import com.rmoya.libreria.R;
 
@@ -17,10 +17,40 @@ public class MainScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
 
+        Button btnAbrirJson = findViewById(R.id.btnAbrirJson);
+        Button btnAbrirBbdd = findViewById(R.id.btnAbrirBbdd);
+        Button btnAbrirFavs = findViewById(R.id.btnAbrirFavs);
+        Button btnAbrirLikes = findViewById(R.id.btnAbrirLikes);
         Button btnCerrarSesion = findViewById(R.id.btnCerrarSesion);
-        btnCerrarSesion.setOnClickListener(v->{
-            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-          sharedPreferences.edit().clear().apply();
+
+        btnAbrirJson.setOnClickListener(v -> {
+            launchActivity(JsonActivity.class);
         });
+
+        btnAbrirBbdd.setOnClickListener(v -> {
+            launchActivity(BBDDActivity.class);
+        });
+
+
+        btnAbrirFavs.setOnClickListener(v -> {
+            launchActivity(FavsActivity.class);
+        });
+        
+        btnAbrirLikes.setOnClickListener(v -> {
+            launchActivity(LikesActivity.class);
+        });
+
+        btnCerrarSesion.setOnClickListener(v -> {
+            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+            sharedPreferences.edit().clear().apply();
+            launchActivity(LoginActivity.class);
+            finish();
+        });
+
+    }
+
+    public void launchActivity(Class clase) {
+        Intent intent = new Intent(getApplicationContext(), clase);
+        startActivity(intent);
     }
 }
