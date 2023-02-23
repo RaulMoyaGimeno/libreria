e9b8693360fe0377a798d98f005546133b7cae19
Alvaro <alvaropuyobraulio@gmail.com>
2023-02-23 16:27:49 +0100

    Activity show book

---
+++ app/src/main/java/com/rmoya/libreria/view/ShowBookActivity.java
@@ -0,0 +1,16 @@
+package com.rmoya.libreria.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.rmoya.libreria.R;
+
+public class ShowBookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_book);
+    }
+}
