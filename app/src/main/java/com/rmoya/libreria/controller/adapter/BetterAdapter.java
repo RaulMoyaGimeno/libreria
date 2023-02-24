32223d6f27825b4672f849c9b488c68baa723def
Snowboys <ismaeljg2002@gmail.com>
2023-02-24 16:01:31 +0100

    Json dificultades

--- app/src/main/java/com/rmoya/libreria/controller/adapter/BetterAdapter.java
+++ app/src/main/java/com/rmoya/libreria/controller/adapter/BetterAdapter.java
@@ -11,39 +11,39 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.rmoya.libreria.R;
-import com.rmoya.libreria.controller.JsonDownloader;
-import com.rmoya.libreria.model.Book;
 import com.rmoya.libreria.model.UserBook;
 import com.rmoya.libreria.view.BBDDActivity;
 
 import java.io.Serializable;
 import java.util.List;
 
-public class JsonAdapter extends RecyclerView.Adapter<JsonAdapter.ViewHolder> implements RegisterAdapter {
 
-    private final List<Book> LibroLista;
+public class BetterAdapter extends RecyclerView.Adapter<BetterAdapter.ViewHolder> implements RegisterAdapter {
 
-    public JsonAdapter(List<Book> libroLista) {LibroLista = libroLista;}
+    private final List<UserBook> LibroLista;
 
+    public BetterAdapter(List<UserBook> libroLista) {LibroLista = libroLista;}
 
     @NonNull
     @Override
-    public JsonAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public BetterAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
         View view = layoutInflater.inflate(R.layout.card_checkbox_layout, parent, false);
-        return new JsonAdapter.ViewHolder(view);
+
+        return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull JsonAdapter.ViewHolder holder, int position) {
-        Book libro = new Book();
+    public void onBindViewHolder(@NonNull BetterAdapter.ViewHolder holder, int position) {
+
+        UserBook libro = new UserBook();
 
 
 
-        holder.txtTitulo.setText(libro.getBook_title());
-        //holder.txtFavs.setText(libro.getFav());
-        holder.txtAutor.setText(libro.getAuthor());
-        //holder.txtLikes.setText(libro.getLiked());
+        holder.txtTitulo.setText(libro.getTitle());
+        holder.txtFavs.setText(libro.getFav());
+        holder.txtAutor.setText(libro.getUser());
+        holder.txtLikes.setText(libro.getLiked());
 
 
         holder.itemView.setOnClickListener(v->{
@@ -52,6 +52,8 @@ public class JsonAdapter extends RecyclerView.Adapter<JsonAdapter.ViewHolder> im
             intent.putExtra("array",libro);
             v.getContext().startActivity(intent);
         });
+
+
     }
 
     @Override
@@ -63,11 +65,13 @@ public class JsonAdapter extends RecyclerView.Adapter<JsonAdapter.ViewHolder> im
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+
         TextView txtTitulo;
         TextView txtFavs;
         CheckBox checkGuardar;
         TextView txtAutor;
         TextView txtLikes;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             txtTitulo = itemView.findViewById(R.id.txtTitulo);
@@ -75,6 +79,7 @@ public class JsonAdapter extends RecyclerView.Adapter<JsonAdapter.ViewHolder> im
             checkGuardar = itemView.findViewById(R.id.checkGuardar);
             txtAutor = itemView.findViewById(R.id.txtAutor);
             txtLikes = itemView.findViewById(R.id.txtLikes);
+
         }
     }
 }
--- app/src/main/java/com/rmoya/libreria/view/JsonActivity.java
+++ app/src/main/java/com/rmoya/libreria/view/JsonActivity.java
@@ -1,25 +1,92 @@
 package com.rmoya.libreria.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 import com.rmoya.libreria.R;
-import com.rmoya.libreria.bbdd.BookADO;
+import com.rmoya.libreria.bbdd.UserBookADO;
 import com.rmoya.libreria.controller.JsonDownloader;
+import com.rmoya.libreria.controller.adapter.BetterAdapter;
+import com.rmoya.libreria.controller.adapter.JsonAdapter;
+import com.rmoya.libreria.controller.adapter.RegisterAdapter;
+import com.rmoya.libreria.model.Book;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class JsonActivity extends AppCompatActivity {
-    public List<BookADO> libro = new ArrayList<>();
+    public String libro;
+    Button btnguardar;
+    public List<Book> lista = new ArrayList<>();
+    public EditText txtbuscar;
+    public CheckBox seleccion;
+    private RecyclerView.Adapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_json);
 
+        txtbuscar = findViewById(R.id.txtBuscarJson);
+        btnguardar = findViewById(R.id.btnGuardarJson);
+
+        Intent intent = getIntent();
+
+        RecyclerView recycler = findViewById(R.id.recyclerJson);
+        recycler.setHasFixedSize(true);
+
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(this, 1);
+        recycler.setLayoutManager(layoutManager);
+
+       libro = intent.getStringExtra(getString(R.string.mostrar));
+        if(libro.equals(getString(R.string.json))){
+            Log.i("fsfs","Hello");
+            Handler handler = new Handler();
+            handler.postDelayed(() -> {
+                new JsonDownloader().execute(lista);
+            }, 3000);
+            adapter = new JsonAdapter(lista);
+        }else if(libro.equals(getString(R.string.likes))){
+         adapter = new BetterAdapter(UserBookADO.orderByFav(this));
+        }else {
+         adapter = new BetterAdapter(UserBookADO.orderByFav(this));
+        }
+        recycler.setAdapter(adapter);
+
+        txtbuscar.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // No se requiere ninguna acción antes de cambiar el texto
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Este método se activará cada vez que el usuario escriba o borre texto en el EditText
+                // Aquí puedes agregar el código para buscar en tu base de datos o servidor los resultados que coincidan con el texto ingresado
+                // Después de obtener los resultados, puedes mostrarlos en una lista o en otro elemento de tu interfaz de usuario
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+
+        });
+
+        btnguardar.setOnClickListener(v->{
+
+
+        });
 
 
     }
