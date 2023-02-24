53380bccbd19b490e700b50f1a393c6718031006
Raúl Moya Gimeno <rmoya889@gmail.com>
2023-02-24 18:12:01 +0100

    BBDD listado

---
+++ app/src/main/java/com/rmoya/libreria/controller/adapter/BBDDAdapter.java
@@ -0,0 +1,86 @@
+package com.rmoya.libreria.controller.adapter;
+
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.rmoya.libreria.R;
+import com.rmoya.libreria.bbdd.BookADO;
+import com.rmoya.libreria.bbdd.UserBookADO;
+import com.rmoya.libreria.model.UserBook;
+import com.rmoya.libreria.view.BBDDActivity;
+
+import java.util.List;
+
+public class BBDDAdapter extends RecyclerView.Adapter<BBDDAdapter.ViewHolder>{
+
+    private final List<UserBook> list;
+
+    public BBDDAdapter(List<UserBook> libroLista) {list = libroLista;}
+
+    @NonNull
+    @Override
+    public BBDDAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.card_bbdd_layout, parent, false);
+
+        return new BBDDAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BBDDAdapter.ViewHolder holder, int position) {
+
+        UserBook userBook = list.get(position);
+        holder.txtTitulo.setText(userBook.getTitle());
+
+        if(userBook.getFav() == 0) holder.cbFavs.setVisibility(View.INVISIBLE);
+        if(userBook.getLiked() <= 0) holder.cbLikes.setVisibility(View.INVISIBLE);
+        if(userBook.getLiked() >= 0) holder.cbDislikes.setVisibility(View.INVISIBLE);
+        if(userBook.getRead() == 0) holder.cbFinished.setVisibility(View.INVISIBLE);
+        if(userBook.getReading() == 0) holder.cbReading.setVisibility(View.INVISIBLE);
+        if(userBook.getDiscard() == 0) holder.cbCancel.setVisibility(View.INVISIBLE);
+
+
+        holder.itemView.setOnClickListener(v->{
+
+            Intent intent = new Intent(v.getContext(), BBDDActivity.class);
+            intent.putExtra("userBook",userBook);
+            intent.putExtra("book", BookADO.getByTitle(v.getContext(), userBook.getTitle()));
+            v.getContext().startActivity(intent);
+        });
+
+
+    }
+
+    @Override
+    public int getItemCount() {return list.size();}
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView txtTitulo;
+        CheckBox cbFavs;
+        CheckBox cbLikes;
+        CheckBox cbDislikes;
+        CheckBox cbCancel;
+        CheckBox cbReading;
+        CheckBox cbFinished;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            txtTitulo = itemView.findViewById(R.id.txtTituloBbdd);
+            cbFavs = itemView.findViewById(R.id.checkBox);
+            cbLikes = itemView.findViewById(R.id.checkBox5);
+            cbDislikes = itemView.findViewById(R.id.checkBox6);
+            cbCancel = itemView.findViewById(R.id.checkBox4);
+            cbReading = itemView.findViewById(R.id.checkBox3);
+            cbFinished = itemView.findViewById(R.id.checkBox2);
+
+        }
+    }
+}
--- app/src/main/java/com/rmoya/libreria/view/BBDDActivity.java
+++ app/src/main/java/com/rmoya/libreria/view/BBDDActivity.java
@@ -1,10 +1,23 @@
 package com.rmoya.libreria.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.TextView;
 
 import com.rmoya.libreria.R;
+import com.rmoya.libreria.bbdd.UserBookADO;
+import com.rmoya.libreria.controller.UserAdapter;
+import com.rmoya.libreria.controller.adapter.BBDDAdapter;
+import com.rmoya.libreria.model.UserBook;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class BBDDActivity extends AppCompatActivity {
 
@@ -12,5 +25,35 @@ public class BBDDActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bbddactivity);
+
+        List<UserBook> books = UserBookADO.getByUser(this, UserAdapter.userStatic);
+
+        RecyclerView recycler = findViewById(R.id.recyclerBbdd);
+        recycler.setHasFixedSize(true);
+
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(this, 1);
+        recycler.setLayoutManager(layoutManager);
+
+        recycler.setAdapter(new BBDDAdapter(books));
+
+        TextView txtBuscar = findViewById(R.id.editTextTextPersonName);
+
+        txtBuscar.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // No se requiere ninguna acción antes de cambiar el texto
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                recycler.setAdapter(new BBDDAdapter(books.stream().filter(userBook -> userBook.getTitle().startsWith(String.valueOf(s))).collect(Collectors.toList())));
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+
+        });
     }
 }
